90707f15fb018c8b14c289469403018a6fc2a335
Goldjay94 <genevieveezeh786@gmail.com>
2023-06-26 17:07:53 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-     /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
-     /* eslint-disable no-unused-vars*/	  
   }
 
   return [task, task2];
