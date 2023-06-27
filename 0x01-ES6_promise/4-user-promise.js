bfb030771ce494f9ec97305233f84c8a347d84e4
Goldjay94 <genevieveezeh786@gmail.com>
2023-06-27 22:14:38 +0000

    Task 4 corrected

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    resolve({ firstName, lastName });
-  });
+  return Promise.resolve({ firstName, lastName });
 }
