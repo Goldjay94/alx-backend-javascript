a3bbb7d7640f86e1c15545a3918ba069ba0d3c47
Ezeibekwe Chioma MaryJane <110990466+Goldjay94@users.noreply.github.com>
2023-06-28 01:54:37 +0100

    task 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,1 +1,9 @@
-console.log ("hello world miss");
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+  
+    return budget;
+  }
