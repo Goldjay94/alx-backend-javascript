0a3b8b69e771e088cb20e4c7378692ab0430e86c
Goldjay94 <genevieveezeh786@gmail.com>
2023-06-29 20:33:05 +0000

    corrected Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,12 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    if (
-      new.target !== Building &&
-      this.evacuationWarningMessage === undefined
-    ) {
-      throw Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
     this._sqft = sqft;
   }
