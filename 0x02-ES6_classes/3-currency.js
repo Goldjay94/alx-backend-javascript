7e043e725d6a8a096d0a1321d5637bd38818f77f
Goldjay94 <genevieveezeh786@gmail.com>
2023-06-29 20:24:50 +0000

    added Task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(co) {
+    this._code = co;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(na) {
+    this._name = na;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
