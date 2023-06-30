ccc0f00369db9a1f80aa434ab128612269b7c7c4
Goldjay94 <genevieveezeh786@gmail.com>
2023-06-30 12:24:41 +0000

    task 10 corrected

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,20 +1,10 @@
+/* eslint-disable */
 import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
-  }
-
-  set range(range) {
-    if (typeof range !== 'string') {
-      throw new TypeError('range must be a string');
-    }
-    this._range = range;
-  }
-
-  get range() {
-    return this._range;
   }
 
   cloneCar() {
