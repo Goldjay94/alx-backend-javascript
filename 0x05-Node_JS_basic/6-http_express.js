032ecb12a9d3c90c60da1a3e3a83c734be7fd3a5
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-21 21:25:14 +0000

    added task 0-7

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+const PORT = process.env.PORT || 1245;
+
+app.listen(PORT, () => {});
+
+module.exports = app;
