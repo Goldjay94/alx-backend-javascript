54a2c49409d9ad69cbd3691862cab2c68e181bbf
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-24 15:34:52 +0000

    added tasks 1-7

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,8 @@
-function calculateNumber (a, b) {
-  return Math.round(a) + Math.round(b);
+function calculateNumber(a, b) {
+    const an = Math.round(a);
+    const bn = Math.round(b);
+    const c = an + bn;
+    return c;
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,18 +1,31 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const assert = require("assert");
 const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
 
-describe('calculateNumber', () => {
-  it('should return 4', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-  it('should return 6', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
+describe("calculateNumber()", function() {
+
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+	assert.strictEqual(res, -2);
+    });
 });
